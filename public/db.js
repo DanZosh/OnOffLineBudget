cbc46f8024600f15747b975f1cdc76dac18184a1
Danny Maier <dannymaier@gmail.com>
2021-02-14 17:36:46 -0800

    Added final functionality to check success and error ro accessing my datastores

--- public/db.js
+++ public/db.js
@@ -19,8 +19,19 @@ request.onupgradeneeded = ({target}) =>{
         // , objectStore.createIndex("transactionIDIndex","transactionID") 
             )
 }
+request.onsuccess = function(event){
+    db = event.target.result
+        console.log(db)
+    if(navigator.online){
+        checkDatabase()
+    }
+}
 
-//write a function to add records to our dataStore
+request.onerror = function(event){
+    console.log(event.target.errorCode)
+}
+
+//write a function to add records to our object Store. This occurs first, because we want to store all our transactions to our indexedDB. then we run checkDatabase which will update the mongoDB if possible.
 function saveRecord(transactionRecord){
     // STEP 1: Open a transaction on my objectStore with read/write capabilities on our `pending` objectStore
        const transaction = db.transaction(["pending"], "readwrite");
@@ -38,20 +49,17 @@ function checkDatabase () {
     const pendingStore = transaction.objectStore("pending")
     //STEP 3: USE the connection to get all the records from the `pending` objectStore and save it to a variable to be used later
     const getAllRecords = pendingStore.getAll();
+        console.log(getAllRecords.result)
+        console.log(JSON.stringify(getAllRecords.result))
 
     //write an event handler for a successful request using the indexedDB `onsuccess` handler:
     getAllRecords.onsuccess = function (event){ //on a successful .getAll()
-        if(getAllRecords.result.length > 0 ){ //if the objectStore isn't empty, use fetch to make a POST of the JSON data found in the object Store to the mongoDatabase using the route described in the api.js file
-            fetch("/api/transaction/bulk",{//add custom settings to the object we're sending to the mongoDB; i'm using the class example here, but this is optional information and we could probably comment it out.
-                method: "POST",
-                body: JSON.stringify(getAllRecords.result),
-                headers:{
-                    Accept:'application/json, text/plain, */*',
-                    'Content-Type': 'application/json',
-                },
-            
+        if(getAllRecords.result.length > 0 ){ //if the objectStore isn't empty, that means we have pending transactions that have been stored in the indexedDB because we're offline. Use fetch to make a POST of the JSON data found in the object Store to the mongoDatabase using the route described in the api.js file and then clear the objectStore of transactions that haven't yet been posted to our database.
+            fetch("/api/transaction/bulk",{
+                method: "POST", //this is posting
+                body: JSON.stringify(getAllRecords.result), //the body of our data is stringified JSON data consisting of the result of our getAllRecords method to our indexedDB            
             })
-            .then((response) => response.json())
+            .then((response) => response.json()) //signals a successful POST
             .then(data => console.log(data))
             .then(() =>{
                 //if successful, open a transaction on my `pending` objectStore and clear out the data using similar syntax to the checkDatabase function above.
