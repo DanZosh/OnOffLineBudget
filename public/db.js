a655806a75476216cd5ca2b7439a39d1e152b19c
Danny Maier <dannymaier@gmail.com>
2021-02-14 19:30:55 -0800

    debugged several things

--- public/db.js
+++ public/db.js
@@ -7,9 +7,9 @@ const request = indexedDB.open("budget_db", 1);
 
 //create an Object Store called `pending` within the onupgradeneeded method, this is essentially our schema
 
-request.onupgradeneeded = ({target}) =>{
-    console.log({target})
-    db = target.result;
+request.onupgradeneeded = function(event){
+    console.log(event)
+    const db = event.target.result;
     db.createObjectStore("pending", {
         //I'm not adding a keypath (primary key equivalent in SQL) here, but i could here if one was needed.
 
@@ -22,7 +22,7 @@ request.onupgradeneeded = ({target}) =>{
 request.onsuccess = function(event){
     db = event.target.result
         console.log(db)
-    if(navigator.online){
+    if(navigator.onLine){
         checkDatabase()
     }
 }
@@ -39,6 +39,7 @@ function saveRecord(transactionRecord){
        const pendingStore = transaction.objectStore("pending")
     //STEP 3: USE the connection to get all the records from the `pending` objectStore and save it to a variable to be used later
        const getAllRecords = pendingStore.add(transactionRecord);
+       console.log("saveRecord success")
 }
 
 //we want to check if our internet connection is working so we can identify what to do with our user's requests
@@ -49,18 +50,19 @@ function checkDatabase () {
     const pendingStore = transaction.objectStore("pending")
     //STEP 3: USE the connection to get all the records from the `pending` objectStore and save it to a variable to be used later
     const getAllRecords = pendingStore.getAll();
-        console.log(getAllRecords.result)
-        console.log(JSON.stringify(getAllRecords.result))
 
     //write an event handler for a successful request using the indexedDB `onsuccess` handler:
-    getAllRecords.onsuccess = function (event){ //on a successful .getAll()
+    getAllRecords.onsuccess = function (){ //on a successful .getAll()
         if(getAllRecords.result.length > 0 ){ //if the objectStore isn't empty, that means we have pending transactions that have been stored in the indexedDB because we're offline. Use fetch to make a POST of the JSON data found in the object Store to the mongoDatabase using the route described in the api.js file and then clear the objectStore of transactions that haven't yet been posted to our database.
             fetch("/api/transaction/bulk",{
                 method: "POST", //this is posting
-                body: JSON.stringify(getAllRecords.result), //the body of our data is stringified JSON data consisting of the result of our getAllRecords method to our indexedDB            
+                body: JSON.stringify(getAllRecords.result), //the body of our data is stringified JSON data consisting of the result of our getAllRecords method to our indexedDB  
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }          
             })
             .then((response) => response.json()) //signals a successful POST
-            .then(data => console.log(data))
             .then(() =>{
                 //if successful, open a transaction on my `pending` objectStore and clear out the data using similar syntax to the checkDatabase function above.
                 const transaction = db.transaction(["pending"], "readwrite");
